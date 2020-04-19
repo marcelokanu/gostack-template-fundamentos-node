44fbaf442bc8cf9a0dd8fcf936443db564360f65
marcelokanu <marcelokanu@gmail.com>
2020-04-19 00:12:30 -0400

    challenge completed

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,25 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    // Verifica se type tem algum dos valores do arrya
+    if (!['income', 'outcome'].includes(type)) {
+      throw Error('Invalid transaction.');
+    }
+
+    if (type === 'outcome' && total < value) {
+      throw Error('Account has insufficient balance for requested action.');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
